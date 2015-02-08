8a1c226fe6fb4fa9d9f7c2ee4b0491674f508838
Nick Delaney <delaney.nick@gmail.com>
2015-02-08 22:54:02 +0000

    Modified students controller so student can be added from user page, listing students in user page

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -9,14 +9,23 @@ class Admin::StudentsController < ApplicationController
 
 	def create
 		@student = Student.new(student_params)
+		@user = params[:user_id]
 		if @student.save
-			redirect_to admin_student_path(@student)
+			redirect_to admin_user_path(@student.user_id)
 		else
 			render 'new'
 		end
 	end
 
+	def destroy
+		@student.destroy
+		redirect_to admin_users_path
+	end
+
 	private
+	def find_student
+		@student = Student.find_by(id: params[:id], franchise_id: current_franchise)
+	end
 
 	def student_params
 		params.require(:student).permit(:first_name,:last_name, :user_id).merge(franchise_id: current_franchise)
